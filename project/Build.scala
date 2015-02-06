ea6e58d1991faaaee88771b18454e3093f11fbe6
Ela Verman <elaverman@gmail.com>
2015-02-06 15:03:26 +0100

    Introduced dependency on torque and slurm.

--- project/Build.scala
+++ project/Build.scala
@@ -3,5 +3,7 @@ import Keys._
 
 object OptimizersBuild extends Build {
    val scCore = ProjectRef(file("../signal-collect"), id = "signal-collect")
-   val scDcopAlgs = Project(id = "dcop-algorithms", base = file(".")) dependsOn(scCore)
+   val scSlurm = ProjectRef(file("../signal-collect-slurm"), id = "signal-collect-slurm")
+   val scTorque = ProjectRef(file("../signal-collect-torque"), id = "signal-collect-torque")
+   val scDcopAlgs = Project(id = "dcop-algorithms", base = file(".")) dependsOn(scCore, scSlurm, scTorque)
 }
